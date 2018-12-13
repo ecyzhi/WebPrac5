9d0e3d8e924193bc99cb4a5fc5a42df2833e69bc
ecyzhi <yeaplz-wa16@student.tarc.edu.my>
2018-12-14 02:56:25 +0800

    Edit roleprovider

--- WebPrac5/Menu.aspx.cs
+++ WebPrac5/Menu.aspx.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Web.Security;
 
 namespace WebPrac5
@@ -12,12 +7,30 @@ namespace WebPrac5
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //MembershipCreateStatus status;
+            //String[] adminRole = new string[] { "Admin" };
+            //String[] userName = new string[] { "admin" };
+            //SqlRoleProvider sqlRoleProvider = new SqlRoleProvider();
+            //SqlMembershipProvider sqlMembershipProvider = new SqlMembershipProvider();
+            //Guid guid = Guid.NewGuid();
+
+
             if (Roles.RoleExists("Admin") == false)
             {
                 Membership.CreateUser("admin", "admin");
                 Roles.CreateRole("Admin");
                 Roles.AddUserToRole("admin", "Admin");
             }
+
+            //if (sqlRoleProvider.RoleExists("Admin") == false)
+            //{
+            //    sqlRoleProvider.CreateRole(adminRole[0]);
+            //    sqlMembershipProvider.CreateUser("admin", "admin", "admin@gmail.com", "", "", true, guid, out status);
+            //    sqlRoleProvider.AddUsersToRoles(userName, adminRole);
+
+            //}
+
+
         }
     }
 }
