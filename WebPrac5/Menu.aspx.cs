26bb490e3c3dbfa4975613b6d5f713180ebe4921
ecyzhi <yeaplz-wa16@student.tarc.edu.my>
2019-01-02 15:06:51 +0800

    update role provider

--- WebPrac5/Menu.aspx.cs
+++ WebPrac5/Menu.aspx.cs
@@ -14,21 +14,38 @@ namespace WebPrac5
             //SqlMembershipProvider sqlMembershipProvider = new SqlMembershipProvider();
             //Guid guid = Guid.NewGuid();
 
-
-            if (Roles.RoleExists("Admin") == false)
+            if (!Roles.RoleExists("Admin"))
             {
                 Membership.CreateUser("admin", "admin");
                 Roles.CreateRole("Admin");
                 Roles.AddUserToRole("admin", "Admin");
             }
 
-            //if (sqlRoleProvider.RoleExists("Admin") == false)
+            //if (!sqlRoleProvider.RoleExists("Admin"))
             //{
             //    sqlRoleProvider.CreateRole(adminRole[0]);
-            //    sqlMembershipProvider.CreateUser("admin", "admin", "admin@gmail.com", "", "", true, guid, out status);
+            //sqlMembershipProvider.CreateUser("admin", "admin", "admin@gmail.com", "", "", true, guid, out status);
             //    sqlRoleProvider.AddUsersToRoles(userName, adminRole);
-
             //}
+
+
+            //try
+            //{
+            //    if (!Roles.RoleExists("admin"))
+            //    {
+            //        Roles.CreateRole("admin");
+            //        //Label1.Text = "Role(s) Created Successfully";
+            //    }
+            //    else
+            //    {
+            //        //Label1k.Text = "Role(s) Already Exists";
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    //Label1.Text = ex.Message;
+            //}
+
 
 
         }
